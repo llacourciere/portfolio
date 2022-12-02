7ac10306ef4c4c2227aa118ae2b5ad515f87540d
LaLacou <108777796+llacourciere@users.noreply.github.com>
2022-12-01 21:03:21 -0800

    added start of footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-import { Nav } from './components';
+import Foot from './components/footer';
 import PortfolioContainer from './components/portfolioContainer';
 
 function App() {
   return (
     <div className="App">
       <PortfolioContainer />
+      <Foot/>
     </div>
   );
 }
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,8 @@
+import "./footer.css"
+const Foot = () => 
+
+<footer>
+<h1 className="footer">Thanks for Stopping By! </h1>
+</footer>
+
+export default Foot;
