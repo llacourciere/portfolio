0cd378641e777a0cfc59e84ab1d54f7866c61aec
LaLacou <108777796+llacourciere@users.noreply.github.com>
2022-12-08 22:01:08 -0800

    fixed about p tags

--- src/Pages/about/About.jsx
+++ src/Pages/about/About.jsx
@@ -1,22 +1,18 @@
 import './about.css';
 import me from './images/about.jpeg';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
 
 
 const About = () =>
-    <div className='about d-flex'>
+    <div className='about'>
         <div className="image sm-12 md-6">
             <img src={me} alt='climbing image' />
         </div>
 
         <div className="bio sm-12 md-6">
-            <p>I am a Portland, Oregon based web developer. I have a passion for making pretty things, whether it is your website or a hand knit sweater.
-                <br></br>
-                <br></br>
-                Over the last year, I have honed skills in HTML, CSS, Javascript, React, Node, SQL, MongoDB and much more!
-                <br></br>
-                <br></br>
-                I might be new to the game, but I live to push my boundaries and work hard!</p>
-
+            <p>I am a Portland, Oregon based web developer. I have a passion for making pretty things, whether it is your website or a hand knit sweater.</p>
+            <p>    Over the last year, I have honed skills in HTML, CSS, Javascript, React, Node, SQL, MongoDB and much more! I might be new to the game, but I live to push my boundaries and work hard!</p>
             <p> Take a look around if you like what you see, let's chat!</p>
         </div>
     </div>
